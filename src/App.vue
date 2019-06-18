1077f2bdc08f122acb1c9abf9ef58c94e96eac39
Raphaël Thériault <raphael_theriault@outlook.com>
2019-06-17 21:15:12 -0400

    Layout draft

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,139 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav
+      class="navbar is-dark"
+      role="navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          is="router-link"
+          to="/"
+        >
+          <img src="https://raw.githubusercontent.com/beat-saber-modding-group/BeatMods-Website/master/src/client/app/layouts/default/logo--mobile.png">
+          <span>BeatMods</span>
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <b-button
+              type="is-light"
+              icon-pack="fab"
+              icon-left="github"
+              outlined
+            >
+              Log in
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="columns">
+      <div class="column is-one-fifth has-background-light">
+        <div class="menu">
+          <p class="menu-label">
+            Browse
+          </p>
+          <ul class="menu-list">
+            <li>
+              <router-link
+                to="/"
+                active-class="is-active"
+                exact
+              >
+                Home
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                to="/mods"
+                active-class="is-active"
+              >
+                Mods
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                to="/about"
+                active-class="is-active"
+              >
+                About
+              </router-link>
+            </li>
+          </ul>
+
+          <p class="menu-label">
+            Account
+          </p>
+          <ul class="menu-list">
+            <li>
+              <router-link
+                to="/user"
+                active-class="is-active"
+                exact
+              >
+                Profile
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                to="/user/settings"
+                active-class="is-active"
+                exact
+              >
+                Settings
+              </router-link>
+            </li>
+          </ul>
+
+          <p class="menu-label">
+            Admin
+          </p>
+          <ul class="menu-list">
+            <li>
+              <router-link
+                to="/settings/accounts"
+                active-class="is-active"
+                exact
+              >
+                Accounts
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                to="/settings"
+                active-class="is-active"
+                exact
+              >
+                Settings
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="column">
+        <router-view/>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  .columns {
+    height: calc(100vh - 2.5rem);
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .menu {
+    padding: 0.75em;
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+Vue.use(Buefy, { defaultIconPack: 'fas' })
 
 Vue.config.productionTip = false
 
