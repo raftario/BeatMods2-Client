9d599500d3ef0fca1e133b79d8f070d6ae1ff251
Raphaël Thériault <raphael_theriault@outlook.com>
2019-06-19 17:52:28 -0400

    Formatting stuff

--- src/components/CustomList.vue
+++ src/components/CustomList.vue
@@ -1,28 +1,40 @@
 <template>
-  <ul v-if="listType === 'unordered'" class="custom-ul">
-    <li v-for="e in contents" v-html="e"></li>
+  <ul
+    v-if="listType === 'unordered'"
+    class="custom-ul"
+  >
+    <li
+      v-for="e in contents"
+      v-html="e"
+    ></li>
   </ul>
-  <ol v-else class="custom-ol">
-    <li v-for="e in contents" v-html="e"></li>
+  <ol
+    v-else
+    class="custom-ol"
+  >
+    <li
+      v-for="e in contents"
+      v-html="e"
+    ></li>
   </ol>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+  import { Component, Prop, Vue } from 'vue-property-decorator'
 
-@Component({})
-export default class CustomList extends Vue {
-  @Prop({
-    required: true
-  })
-  public contents: string[]
+  @Component({})
+  export default class CustomList extends Vue {
+    @Prop({
+      required: true
+    })
+    public contents: string[]
 
-  @Prop({
-    default: 'unordered',
-    validator (value: string): boolean {
-      return ['unordered', 'ordered'].indexOf(value) !== -1
-    }
-  })
-  public listType: string
-}
+    @Prop({
+      default: 'unordered',
+      validator (value: string): boolean {
+        return ['unordered', 'ordered'].indexOf(value) !== -1
+      }
+    })
+    public listType: string
+  }
 </script>
--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="markdown" v-html="htmlContents"></div>
+  <div
+    class="markdown"
+    v-html="htmlContents"
+  ></div>
 </template>
 
 <script lang="ts">
-import { sanitize } from 'dompurify'
-import marked from 'marked'
-import { Component, Prop, Vue } from 'vue-property-decorator'
+  import { sanitize } from 'dompurify'
+  import marked from 'marked'
+  import { Component, Prop, Vue } from 'vue-property-decorator'
 
-@Component({})
-export default class Markdown extends Vue {
-  @Prop({
-    required: true
-  })
-  public contents: string
-
-  @Prop()
-  public baseUrl: string
-
-  get htmlContents (): string {
-    return marked(this.contents, {
-      baseUrl: this.baseUrl,
-      sanitize: true,
-      sanitizer: sanitize
+  @Component({})
+  export default class Markdown extends Vue {
+    @Prop({
+      required: true
     })
+    public contents: string
+
+    @Prop()
+    public baseUrl: string
+
+    get htmlContents (): string {
+      return marked(this.contents, {
+        baseUrl: this.baseUrl,
+        sanitize: true,
+        sanitizer: sanitize
+      })
+    }
   }
-}
 </script>
