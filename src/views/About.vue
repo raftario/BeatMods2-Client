11af58a3a47ad92ac63665f9eaf1c8d124083dc4
Raphaël Thériault <raphael_theriault@outlook.com>
2019-06-21 21:56:16 -0400

    Basic about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="section">
+      <h1 class="title is-1">About</h1>
+      <hr>
+      <p class="mb">
+        BeatMods is maintained free of charge by the Beat Saber Modding Group.
+        All mods present on the site are also created for free by community members.
+      </p>
+      <p>
+        BeatMods itself is composed of two parts:
+      </p>
+      <ul class="mb custom-ul">
+        <li>The <a href="https://github.com/nike4613/BeatMods2">BeatMods API</a>, written in C++ using Restbed and distributed under the GNU General Public License v3.0</li>
+        <li>The <a href="https://github.com/raftario/BeatMods2-Client">BeatMods web client</a>, written in TypeScript using Vue and distributed under the MIT License</li>
+      </ul>
+      <p>
+        You can contribute to both projects on GitHub.
+      </p>
+    </section>
   </div>
 </template>
